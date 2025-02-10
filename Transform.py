b73c6541aff90b7379cac8c03a6a671212a4b62c
RuoxueWang-lab <ruoxue.wang@outlook.com>
2025-02-10 13:48:56 +0000

    Edited

--- Transform.py
+++ Transform.py
@@ -6,6 +6,10 @@ class TransformSubset(torch.utils.data.Dataset):
         self.transform = transform
         
     def __len__(self):
+        """
+        DataLoader would need the length of the subset. 
+        Pytorch will call this function automatically when we pass our instance into DataLoader.
+        """
         return len(self.subset)
     
     def __getitem__(self, idx):
